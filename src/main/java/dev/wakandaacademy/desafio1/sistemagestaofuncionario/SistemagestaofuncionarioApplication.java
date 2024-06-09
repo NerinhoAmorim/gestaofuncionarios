05cf3469e838a1207b6c12ad899e41c3bac2b96a
Nerinho Nascimento <neramornas@gmail.com>
2024-06-09 01:25:26 -0300

    incio desafio1
    Signed-off-by: Nerinho Nascimento <neramornas@gmail.com>

---
+++ src/main/java/dev/wakandaacademy/desafio1/sistemagestaofuncionario/SistemagestaofuncionarioApplication.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.desafio1.sistemagestaofuncionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemagestaofuncionarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemagestaofuncionarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/wakandaacademy/desafio1/sistemagestaofuncionario/SistemagestaofuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wakandaacademy.desafio1.sistemagestaofuncionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemagestaofuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
